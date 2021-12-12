967144396f12b0833e45d67721a97a288149afb3
Marcin Nahajowski <immortalmar0@gmail.com>
2021-12-12 12:13:24 +0100

    Modify import structure

--- import_data.py
+++ import_data.py
@@ -35,15 +35,8 @@ def load_default_dataset():
 def get_filenames():
     if config.content['custom_covid_data_path'] == '':
         filenames = tf.io.gfile.glob(str(DEFAULT_COVID_DATA_PATH))
-        n = int(round(0.3 * len(filenames)))
-        # filenames = filenames[:len(filenames) - n]
-        # filenames.extend(tf.io.gfile.glob(str(DEFAULT_NON_COVID_DATA_PATH)))
-        m = int(round(0.3 * len(tf.io.gfile.glob(str(DEFAULT_NON_COVID_DATA_PATH)))))
         data = tf.io.gfile.glob(str(DEFAULT_NON_COVID_DATA_PATH))
-        # data = data[:len(data) - m]
         filenames.extend(data)
-        # filenames = remove_invalid_files(filenames=filenames)
-        # filenames = filenames[:len(filenames) - m]
     else:
         filenames = tf.io.gfile.glob(str(config.content['custom_covid_data_path'] + '*'))
         data = tf.io.gfile.glob(str(config.content['custom_non_covid_data_path'] + '*'))
